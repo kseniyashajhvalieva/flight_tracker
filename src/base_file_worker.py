2540f9a6b215a5f9870b8121068df8e71da8c8bb
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-03-13 12:38:32 +0300

    Добавлен абстрактный класс BaseFileWorker

---
+++ src/base_file_worker.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict, Any
+
+
+class BaseFileWorker(ABC):
+    """Абстрактный базовый класс для работы с файлами."""
+
+    @abstractmethod
+    def add_data(self, data: Dict[str, Any]) -> None:
+        """Добавить информацию о самолёте в файл."""
+        pass
+
+    @abstractmethod
+    def get_data(self, **criteria) -> List[Dict[str, Any]]:
+        """Получить данные из файла по указанным критериям."""
+        pass
+
+    @abstractmethod
+    def delete_data(self, **criteria) -> None:
+        """Удалить информацию о самолётах по критериям."""
+        pass
